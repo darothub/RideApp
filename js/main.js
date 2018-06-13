d694eab8019dcb7f196af97b1ec1fc5a089f732e
darothub <darotudeen@gmail.com>
2018-06-13 11:55:01 +0100

    profile page and offer creation

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
-const user = document.getElementById("user");
-const password = document.getElementById("password")
+var user = document.getElementById("user");
+var password = document.getElementById("password")
 var attempt = 3
 
 const validate=()=>{
-    if(user.value !=="darot" && password.value !== "Youngster1"){
+        
+        if(user.value =="darot" && password.value == "Youngster1"){
+            window.open('http://127.0.0.1:5500/RideApp/rideoffer.html')
+            return false
+            
+        }
         attempt--
+        
         alert("You have " + attempt +  " attempts to go")
-    }
+        if(attempt == 0){
+            alert("please confirm your login details and try again")
+            location.reload()
+            return false
+        }
+        
     
-    else{
-        window.open('http://127.0.0.1:5500/RideApp/rideoffer.html')
-    }
+    
     
 }
