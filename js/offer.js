2cf014178ddea2094771706e798b9b771845bad9
darothub <darotudeen@gmail.com>
2018-06-14 16:42:22 +0100

    adjusting profile page with hoverable nav

--- js/offer.js
+++ js/offer.js
@@ -1,9 +1,1 @@
-var offerModal = document.getElementById("offerModal");
-var modallink = document.getElementsByClassName("modallink");
-var closeBtn = document.getElementsByClassName("closeBtn");
 
-modallink.addEventListener('click', openOffer);
-
-const openOffer=()=>{
-    offerModal.style.display ='block';
-}
