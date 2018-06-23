a9368bcb4f57d86a851fac5bd587f4f257635724
darothub <darotudeen@gmail.com>
2018-06-23 23:57:47 +0100

    heroku deployment

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var path = require('path');
 var fs = require('fs');
 var data = fs.readFileSync('data.json');
 var rideArray = JSON.parse(data);
@@ -9,7 +8,7 @@ var serveStatic = require('serve-static');
 var port = process.env.port || 3000;
 
 
-app.use('/api/v1', serveStatic(path.join(__dirname, 'views')))
+//app.use('/api/v1', serveStatic(path.join(__dirname, 'views')))
 
 app.get('/api/v1/rides', getRideOffers=(req, res)=>{
     res.send(rideArray)
