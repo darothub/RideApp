7870c87cfa2372eea790ecf331c190a3802c85ba
darothub <darotudeen@gmail.com>
2018-06-26 12:04:36 +0100

    This will enable users to access all end point

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+var allRides=[
+    {
+        id:1,
+        name:"Darot",
+        from: "Mushin",
+        to:"Lagos",
+        time: 16/4/2018    
+
+    },
+    {
+        id:2,
+        name:"Darot",
+        from: "Mushin",
+        to:"Lagos",
+        time: 16/4/2018    
+
+    },
+    {
+        id:3,
+        name:"Darot",
+        from: "Mushin",
+        to:"Lagos",
+        time: 16/4/2018    
+
+    },
+   
+]
+
+
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -16,27 +45,55 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.use('/api/v1', serveStatic(path.join(__dirname, 'views')))
 
 
-app.post('/api/v1/Registration', urlencodedParser, function(req, res){
+app.post('/api/v1/Registration', urlencodedParser, postRegistration=(req, res)=>{
+    var bodyF = req.body
     var password = req.body.password
     var rpassword = req.body.repeat_password
     if(password === rpassword){
-        var bodyF = req.body
+        console.log("its ok")
     }
     else{
         console.log("password does not match")
     }
-    
-    
+    const saved =(err)=>console.log("ok")
+    fs.writeFile('data.json', JSON.stringify(bodyF, null, 2), saved)
+    allRides.push(bodyF)    
     res.send(bodyF)
+    console.log(allRides)
 });
 
 app.get('/api/v1/rides', getRideOffers=(req, res)=>{
-    res.send(rideArray)
+    res.send(allRides)
 });
 
 app.get('/api/v1/rides/:id', getRideOffers=(req, res)=>{
-    let id = req.params.id
-    res.send("Here are ride offers for " + id)
+    let ids = req.params.id
+    for(var i =0; i<allRides.length; i++){
+        if(allRides[i].id == ids){
+            res.send(allRides[i])
+        }
+    }
+});
+
+app.post('/api/v1/rides', urlencodedParser, postRideOffers=(req, res)=>{
+    var rideOffer = req.body
+
+    res.send(rideOffer)
+
+});
+app.post('/api/v1/rides/:id/:requests',  postRideOffers=(req, res)=>{
+    let request={
+        id:req.params.id,
+        action:req.params.requests
+    }
+    var reply={
+        message :"thank you"
+    }
+
+    res.send(reply)
+   
+    console.log("Thank you")
+
 });
 
 const port = process.env.PORT || 8080;
