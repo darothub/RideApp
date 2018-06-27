b8997db23a10b4d1dfcad73d0afbfcebc715c556
darothub <darotudeen@gmail.com>
2018-06-27 12:56:21 +0100

    setup heroku

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res)=> res.send('Hello World!'));
+app.use('/', (req, res)=> res.send('Hello World!'));
 
 const run=()=> console.log("way to go server!")
 app.listen(PORT, run)
