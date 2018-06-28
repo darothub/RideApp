dc37d42dc5417cdd92b3dd100f93dd7f6cb17c1d
darothub <darotudeen@gmail.com>
2018-06-28 11:22:18 +0100

    chore(setup): integrate travis, add test and coverage badges
    [Finishes #158668484]

--- server/index.js
+++ server/index.js
@@ -4,5 +4,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use('/', (req, res)=> res.send('Hello World!'));
 
-const run=()=> console.log("way to go server!")
-app.listen(PORT, run)
+const run=()=> console.log("way to go server!");
+app.listen(PORT, run);
+
+module.exports = app;
