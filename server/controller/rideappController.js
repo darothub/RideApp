487f415b4f8c3fc85fba9ada66b2adecb1c76c1c
darothub <darotudeen@gmail.com>
2018-06-29 07:31:35 +0100

    add create-ride-offer-endpoint

---
+++ server/controller/rideappController.js
@@ -0,0 +1,20 @@
+const rides = [{
+  driverName: 'Darot',
+  destination: 'Ajah',
+  departure: 'Abule-egba',
+  departureDate: new Date().toLocaleDateString(),
+  departureTime: new Date().toLocaleTimeString(),
+  request: [],
+}];
+
+const createRide = (req, res) => {
+  if (req.body === {}) {
+    res.send('Error: You have entered nothing');
+  }
+  rides.push(req.body);
+  res.sendStatus(201);
+};
+
+const func = { createRide };
+
+export default func;
---
+++ server/routes/routers.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+import func from '../controller/rideappController';
+
+const router = express.Router();
+
+router.get('/api/v1', (req, res) => res.send('Home page'));
+router.post('/api/v1/rides', func.createRide);
+
+export default router;
