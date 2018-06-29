c1f02e84c7f8d072569a3c206892f1192696d461
darothub <darotudeen@gmail.com>
2018-06-29 08:58:25 +0100

    re-arrangement on gh-pages

--- js/main.js
+++ js/main.js
@@ -3,20 +3,21 @@ var password = document.getElementById("password")
 var attempt = 3
 
 const validate=()=>{
+    window.open('http://127.0.0.1:5500/profile.html')
         
-        if(user.value =="darot" && password.value == "Youngster1"){
-            window.open('http://127.0.0.1:5500/RideApp/profile.html')
-            return false
+    /**if(user.value =="darot" && password.value == "Youngster1"){
+        window.open('http://127.0.0.1:5500/profile.html')
+        return false
             
-        }
-        attempt--
+    }
+    attempt--
         
-        alert("You have " + attempt +  " attempts to go")
-        if(attempt == 0){
-            alert("please confirm your login details and try again")
-            location.reload()
-            return false
-        }
+    alert("You have " + attempt +  " attempts to go")
+    if(attempt == 0){
+        alert("please confirm your login details and try again")
+        location.reload()
+        return false
+    }**/
         
     
     
--- js/offer.js
+++ js/offer.js
@@ -1,1 +1,21 @@
+var myOffer = document.getElementById("myoffer");
+var closeBtn = document.getElementsByClassName("close")[0];
+var cancelBtn = document.getElementById("cancel");
+
+const openOffer=()=>{
+    myOffer.style.display = "block"
+
+}
+
+closeBtn.addEventListener('click', function(){
+    myOffer.style.display = "none";
+})
+
+cancelBtn.addEventListener('click', function(){
+    myOffer.style.display = "none";
+})
+/*cancelBtn.onclick = function() {
+    myOffer.style.display = "none";
+}*/
+
 
